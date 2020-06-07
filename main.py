97b09b2b8c666d96786743a3019fdef10a9dd7a6
rigc <riqui.garcia@gmail.com>
2020-06-07 14:14:09 -0500

    adding demo functionality

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-def main:
-    print("Hello")
+import os
+from flask import Flask
+app = Flask(__Name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/status')
+def hello():
+    return 'I am good'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0.", port=8080)
